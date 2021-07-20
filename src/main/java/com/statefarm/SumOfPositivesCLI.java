1eff5eb32743cf3addbb6244f6725683a4b5b837
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-07-20 10:15:30 -0500

    computed sum of positives

--- src/main/java/com/statefarm/SumOfPositivesCLI.java
+++ src/main/java/com/statefarm/SumOfPositivesCLI.java
@@ -4,11 +4,13 @@ public class SumOfPositivesCLI {
 
     public static void main(String[] args) {
 
+        int sumOfPositives = 0;
         for (int i = 0; i < args.length; i++) {
 
             if (Integer.parseInt(args[i]) > 0) {
-                System.out.println(args[i]);
+                sumOfPositives += Integer.parseInt(args[i]);
             }
         }
+        System.out.println(sumOfPositives);
     }
 }
