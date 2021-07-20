1c6afb8bc3bf8f7594fde4764da3e0327e432b8d
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-07-20 10:02:24 -0500

    created loop for args

--- src/main/java/com/statefarm/SumOfPositivesCLI.java
+++ src/main/java/com/statefarm/SumOfPositivesCLI.java
@@ -1,4 +1,11 @@
 package com.statefarm;
 
 public class SumOfPositivesCLI {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
 }
